3f654acb15d8f1b9075371ca2d72aab62d36bbc9
Veera Phani <veeraphani21@gmail.com>
2022-12-14 07:16:37 +0530

    first commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+   private String name;
+
+   public void SetName(String n){
+      name = n;
+   }
+   public String getName(){
+      return name;
+   }
     
 }
