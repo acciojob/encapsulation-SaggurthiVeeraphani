d13bfa4fd772a893d679d6c91f4097ee5a52023a
Veera Phani <veeraphani21@gmail.com>
2022-12-14 09:23:56 +0530

    final21

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,8 +3,8 @@ package com.driver;
 public class RWOnly {
    private String name;
 
-   public void SetName(String n){
-      name = n;
+   public void SetName(String name){
+      this.name = name;
    }
    public String getName(){
       return name;
